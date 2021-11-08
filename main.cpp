a3f260b27a521e51382c5d9b012dec9447052461
rmerrie <rmerrie@student.21-school.ru>
2021-11-08 06:01:34 -0800

    1

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@ int	main()
 	Map<int, std::string> myMap;
 	ft::pair<int, std::string> pair1(2, "Notebook");
 	std::cout << pair1.first << ' ' << pair1.second << std::endl;
-	/*myMap.insert(std::make_pair(1, "phone"));
+	std::map test1;
+	myMap.insert(std::make_pair(1, "phone"));
 	myMap.insert(ft::pair<int, std::string>(2, "notebook"));
 	std::cout << myMap[1] << std::endl;
 	myMap[18] = "TV";
-	std::cout << myMap[18] << std::endl;*/
+	std::cout << myMap[18] << std::endl;
 	return 0;
 }
